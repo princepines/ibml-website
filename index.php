16cd0509ab107f295ab157b9a72e665dea262725
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-06-06 19:26:03 +0000

    inital commit

--- index.php
+++ index.php
@@ -7,16 +7,21 @@
     <title>TechSPARKS Interbarangay ML Tournament 2023</title>
     <link rel="stylesheet" href="style.css">
     <link rel="icon" type="image/x-icon" href="white.png">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 </head>
 <body>
     <?php include 'nav.php';?>
     <div class="container">
         <div class="row">
             <div class="col">
-                <h1>Interbarangay ML Tournament 2023</h1>
+                <img src="poster.png" alt="Poster of IBML">
                 <!-- Insert Live Stats -->
                 <!-- Insert BracketHQ -->
-                <button class="btn btn-primary">Watch Now!</button>
+                <button type="button" class="btn btn-warning ml-2" onclick="window.location.href='k4th.mooo.com;">Watch Now!</button>
             </div>
         </div>
 </body>
---
+++ nav.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#"><img src="techsparks.png" alt="TechSparks Logo" width="30px"></a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+            <a class="nav-item nav-link" href="/">Home</a>
+            <a class="nav-item nav-link" href="https://k4th.mooo.com" target="_blank">Watch Live</a>  
+            <a class="nav-item nav-link" href="about.php">About</a> 
+     </div>
+  </div>
+</nav>
