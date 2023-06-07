a05275f25c45fc4490cd74a239340e579c4297f6
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-06-07 04:09:00 +0000

    stats

--- index.php
+++ index.php
@@ -24,7 +24,8 @@
                 <h1>Live Leaderboard</h1>
                 <iframe src="https://brackethq.com/b/j1ljb/embed/" width="90%" height="300" frameborder="0"></iframe>
                 <br>
-                <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vS6e2JdRGCaFi8MzW43Zq4o0iqAY6CkVJfOiKcZHMW_9IByeyPKwffxZoapU4_OwSfnT-RxgmGWdvGI/pubhtml?widget=true&amp;headers=false"></iframe>
+                <h1>Live Statistics</h1>
+                <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vS6e2JdRGCaFi8MzW43Zq4o0iqAY6CkVJfOiKcZHMW_9IByeyPKwffxZoapU4_OwSfnT-RxgmGWdvGI/pubhtml?widget=true&amp;headers=false" width="100%" height="300"></iframe>
             </center>
             <button type="button" class="btn btn-warning ml-2" onclick="window.location.href='https://k4th.mooo.com';">Watch Now!</button>
         </div>
