ac41482607a4fa4dc2293b6bd8beb593c232f00c
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-06-07 04:15:19 +0000

    scale sheets

--- index.php
+++ index.php
@@ -25,7 +25,7 @@
                 <iframe src="https://brackethq.com/b/j1ljb/embed/" width="90%" height="300" frameborder="0"></iframe>
                 <br><br>
                 <h1>Live Statistics</h1>
-                <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vS6e2JdRGCaFi8MzW43Zq4o0iqAY6CkVJfOiKcZHMW_9IByeyPKwffxZoapU4_OwSfnT-RxgmGWdvGI/pubhtml?widget=true&amp;headers=false" width="100%" height="300"></iframe>
+                <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vS6e2JdRGCaFi8MzW43Zq4o0iqAY6CkVJfOiKcZHMW_9IByeyPKwffxZoapU4_OwSfnT-RxgmGWdvGI/pubhtml?widget=true&amp;headers=false" width="100%" height="300" frameborder="0"></iframe>
             </center>
             <button type="button" class="btn btn-warning ml-2" onclick="window.location.href='https://k4th.mooo.com';">Watch Now!</button>
         </div>
