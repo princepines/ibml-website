80f08e3ea36dbadea10757af57337434a07fd198
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-06-07 09:47:55 +0000

    br

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
                 <h1>Live Statistics</h1>
                 <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vS6e2JdRGCaFi8MzW43Zq4o0iqAY6CkVJfOiKcZHMW_9IByeyPKwffxZoapU4_OwSfnT-RxgmGWdvGI/pubhtml?widget=true&amp;headers=false" width="100%" height="300" frameborder="0"></iframe>
             </center>
-        </div>
+        </div><br>
         <div class="row">
             <button type="button" class="btn btn-warning ml-2" onclick="window.location.href='https://k4th.mooo.com';">Watch Now!</button>
         </div>
