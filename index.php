6f8694b087f4ba4e1ea80807aed8bcdf97a83ac9
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-06-09 08:19:39 +0000

    logo to techsparks

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TechSPARKS Interbarangay ML Tournament 2023</title>
     <link rel="stylesheet" href="style.css">
-    <link rel="icon" type="image/x-icon" href="white.png">
+    <link rel="icon" type="image/x-icon" href="techsparks.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
