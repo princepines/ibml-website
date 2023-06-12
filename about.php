5b350dc156c21d2a7c2a6b80adfa9bfdf0d137ea
Frizth Lyco Tatierra <lycol50@gmail.com>
2023-06-12 14:50:07 +0000

    update about

--- about.php
+++ about.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -22,6 +23,7 @@
     <!-- Include this to make the og:image larger -->
     <meta name="twitter:card" content="summary_large_image">
 </head>
+
 <body>
     <!-- Messenger Chat Plugin Code -->
     <div id="fb-root"></div>
@@ -31,27 +33,28 @@
     </div>
 
     <script>
-      var chatbox = document.getElementById('fb-customer-chat');
-      chatbox.setAttribute("page_id", "113358421781727");
-      chatbox.setAttribute("attribution", "biz_inbox");
+    var chatbox = document.getElementById('fb-customer-chat');
+    chatbox.setAttribute("page_id", "113358421781727");
+    chatbox.setAttribute("attribution", "biz_inbox");
     </script>
 
     <!-- Your SDK code -->
     <script>
-      window.fbAsyncInit = function() {
+    window.fbAsyncInit = function() {
         FB.init({
-          xfbml            : true,
-          version          : 'v17.0'
+            xfbml: true,
+            version: 'v17.0'
         });
-      };
+    };
 
-      (function(d, s, id) {
+    (function(d, s, id) {
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
+        js = d.createElement(s);
+        js.id = id;
         js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
         fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
+    }(document, 'script', 'facebook-jssdk'));
     </script>
     <?php include 'nav.php';?>
     <div class="container">
@@ -62,38 +65,73 @@
                 <br>
                 <h1>Tournament Rules</h1>
                 <ol>
-                    <li>Each Team must consist of members from barangays in any city, team captain and members must be from Grade 10 and Grade 12 Students. (Captain and Members must present their School ID upon orientation). No limit of teams joining per barangay/city.</li>
-                    <li>Each team must have no more than 6 members. (One player is reserved for substitution purposes)</li>
-                    <li>Each team must have 1 captain that will be responsible for his/her members behavior and attendance during the event matches.</li>
-                    <li>Each team, should follow the registration deadline and the dates of the tournament:<br>June 1 – 11 Registration<br>June 12 – Orientation<br>June 14 – 22 Online Elimination<br>June 23 Championship (Onsite – Informatics College Northgate Alabang Campus)</li>
-                    <li>Each player must provide their own devices including Pocket WiFi/Data (if needed) and mobile devices.</li>
-                    <li>No player substitution is allowed once team has been registered.</li>
-                    <li>ONLY 1 account can be used, it is required to register the account/IGN and must be used all throughout the tournament.</li>
+                    <li>Each Team must consist of members from barangays in any city, team captain and members must be
+                        from Grade 10 to Grade 12 Students. (Captain and Members must present their School ID upon
+                        orientation). No limit of teams joining per barangay/city.</li>
+                    <li>Each team must consist of exactly 6 members, with one player reserved for substitution purposes.
+                    </li>
+                    <li>Each team must have 1 captain that will be responsible for his/her members behavior and
+                        attendance during the event matches.</li>
+                    <li>Each team, should follow the registration deadline and the dates of the tournament:<br>
+                        June 1 – 11 Registration (June 11, 12am - Closed of Registration)<br>
+                        June 12 – Orientation<br>
+                        June 14 – 22 Online Elimination<br>
+                        June 23 Championship (Onsite – Informatics College Northgate Alabang Campus)</li>
+                    <li>Each player must provide their own devices including Pocket WiFi/Data (if needed) and mobile
+                        devices.</li>
+                    <li>No player substitution is allowed once a team has been registered.</li>
+                    <li>ONLY 1 account can be used, it is required to register the account/IGN and must be used all
+                        throughout the tournament.</li>
                     <li>No registration fee will be collected.</li>
-                    <li>Latecomers will not be entertained to play. Incomplete teams will automatically be disqualified for that match. Teams must check-in 15 minutes before the team’s scheduled match.</li>
-                    <li>All types of communication within each team is allowed, BUT any form of profanity is strictly prohibited (either by means of vocal or chat communication: only 2 warnings per team and disqualification on 3rd warning).</li>
-                    <li>During event, each team must gather into one place, conduct a video-conference call for each team to ensure that the game is fairly played. Meeting links will be provided.</li>
-                    <li>Players are prohibited from using 3rd party software or cheats, violating this rule will get the team disqualified immediately.</li>
+                    <li>Latecomers will not be entertained to play. Incomplete teams will automatically be disqualified
+                        for that match. Teams must check-in 15 minutes before the team’s scheduled match.</li>
+                    <li>All types of communication within each team is allowed, BUT any form of profanity is strictly
+                        prohibited (either by means of vocal or chat communication: only 2 warnings per team and
+                        disqualification on 3rd warning).</li>
+                    <li>During the event, each team must gather into one place, conduct a video-conference call for each
+                        team to ensure that the game is fairly played. Meeting links will be provided.</li>
+                    <li>Players are prohibited from using 3rd party software or cheats, violating this rule will get the
+                        team disqualified immediately. (This includes MapHack)</li>
                 </ol>
                 <br>
                 <h1>Game Mechanics</h1>
                 <ol>
-                    <li>The game will be in draft pick, 3 heroes will be banned per side of team during the match.</li>
-                    <li>Team opponents will be randomly assigned.</li>
-                    <li>There will be 3 games per match, the first team to get 2 wins is considered the winner of the match and proceed to the next match.</li>
-                    <li>There will be an Elimination Round which will happen online, and Finals will be held face-to-face.</li>
+                    <ol>
+                        <li>The game will be in draft pick, 3 heroes will be banned per side of the team during the
+                            match.</li>
+                        <li>Proper sequence of game:</li>
+                        <ul>
+                            <li>There will be 1 game on Elimination Rounds.</li>
+                            <li>Best of Three in Semi-Finals (including playoff for 3rd place).</li>
+                            <li>First to Three on Finals Match.</li>
+                        </ul><br>
+                        <li>There will be an Elimination Rounds including Semi-Finals which will happen online, and
+                            Finals will be held face-to-face.</li>
+                    </ol>
                 </ol>
                 <br>
                 <h1>Pay-Per-View Guideline</h1>
                 <ol>
-                    <li>Pay-Per-View event will be available for the whole event week.</li>
-                    <li>Pay-Per-View is available to anyone but will have to pay PHP30 pesos. Onsite Finals Match will be screened and must provide a receipt that you have availed the Pay-Per-View.</li>
-                    <li>Photography is allowed, but Videography OR any type of capturing moving pictures is NOT Allowed, violating this may result on person be kicked out by our Marshalls.</li>
-                    <li>This will be Livestreamed through Online Pay-Per-View ONLY.</li>
-                    <li>Finals can be watched too physically on Informatics Northgate Campus (present your Screenshot Confirmation that you have been paid for this event).</li>
+                    <li>The Pay-Per-View will be available for the whole event week.</li>
+                    <li>Pay-Per-View is available to anyone. In order to watch the livestream event, viewers shall avail
+                        the slots. The cost of pay-per-view as follows:</li>
+                    <ul>
+                        <li>30PHP for Elimination Rounds</li>
+                        <li>40PHP for Semi-Finals</li>
+                        <li>50PHP for Finals</li>
+                    </ul>
+                    <li>Photography is permitted, but videography or any form of capturing moving pictures is strictly
+                        prohibited. Violation of this rule may result in immediate removal from the premises by our
+                        marshals.</li>
+                    <li>This event will be exclusively live streamed through online pay-per-view. Additionally, for the
+                        finals, there will be an audience room available for a physical event.</li>
+                    <li>Finals can be watched physically on Informatics Northgate Campus. Must observe the following
+                        instructions:</li>
+                    <li>Present your Screenshot Confirmation that you have been paid for the finals match event.</li>
                     <li>Any types of cheering signs are allowed but maintain CLAYGO. (For Physical Watching)</li>
                 </ol>
             </div>
         </div>
 </body>
+
 </html>
